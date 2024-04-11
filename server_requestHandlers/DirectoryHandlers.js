6be8e59073a1cced8a31b316821fdc2fe16fcad4
ZeilingerAlexander <116539631+ZeilingerAlexander@users.noreply.github.com>
2024-04-11 06:18:22 +0200

    ~ moved Handle get directory navigator to DirectoryHandlers.js

--- server_requestHandlers/DirectoryHandlers.js
+++ server_requestHandlers/DirectoryHandlers.js
@@ -1,8 +1,21 @@
 // Handles interaction with directories
 
 /*Handles getting the Directory Navigator page, call this whenever user goes to directory*/
-export async function HandleGetDirectory(req,res){
-    return new Promise(async (resolve, reject) => {
-        
+import {HandleGetFile} from "./FileHandlers.js";
+
+export async function HandleGetDirectoryNavigator(req, res){
+    return new Promise( async (resolve, reject) => {
+        // exprect the directory navigator to exist under the provided directory in env
+        // if returning it doesnt work reject
+        req.url = process.env.DIRECTORYNAVIGATOR_HTML_RELATIVEPATH;
+        const success_message = await HandleGetFile(req, res).catch(
+            (err) => console.log(err)
+        );
+        if (success_message){
+            return resolve("Successfully got Directory Navigator");
+        }
+        else{
+            return reject("Failed to get Directory Navigator");
+        }
     });
 }
--- server_requestHandlers/FileHandlers.js
+++ server_requestHandlers/FileHandlers.js
@@ -44,21 +44,3 @@ export async function HandleNotFound(req,res){
         }
     });
 }
-
-/*Handles returning the Directory Navigator*/
-export async function HandleGetDirectoryNavigator(req,res){
-    return new Promise( async (resolve, reject) => {
-        // exprect the directory navigator to exist under the provided directory in env
-        // if returning it doesnt work reject
-        req.url = process.env.DIRECTORYNAVIGATOR_HTML_RELATIVEPATH;
-        const success_message = await HandleGetFile(req, res).catch(
-            (err) => console.log(err)
-        );
-        if (success_message){
-            return resolve("Successfully got Directory Navigator");
-        }
-        else{
-            return reject("Failed to get Directory Navigator");
-        }
-    });
-}
