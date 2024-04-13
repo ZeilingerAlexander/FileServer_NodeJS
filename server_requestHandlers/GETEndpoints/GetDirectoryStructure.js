c9e6f078225815c47a7464d0f04c8afbc1cbd516
HomePC <alexanderzeilinger06@gmail.com>
2024-04-13 09:59:37 +0200

    added getting directory location of request

--- server_requestHandlers/GETEndpoints/GetDirectoryStructure.js
+++ server_requestHandlers/GETEndpoints/GetDirectoryStructure.js
@@ -1,6 +1,28 @@
 // /GET/GetDirectoryStructure
-export async function HandleGetDirectoryStructure(req,res){
+import * as url from "node:url";
+import {GetUrlParameters} from "../../InputValidator.js";
+import {LogErrorMessage} from "../../logger.js";
+
+export async function HandleGetDirectoryStructure(req, res){
     return new Promise( async (resolve, reject) => {
+        const urlParams = await GetUrlParameters(req.url).catch(
+            (err) => LogErrorMessage(err.message, err)
+        );
+        if (!urlParams){
+            return reject("Failed to get url parameters");
+        }
+        
+        // get the val entry of url paramaters since thats where directory location resides
+        const dirLocation = urlParams["val"];
+        if (!dirLocation) {
+            return reject("Url Paramter val not found, cant get directory location");
+        }
+        
         
     });
+}
+
+/*Gets the "var" property of the url provided if there is one*/
+async function GetDirectoryStructurePathFromUrlParameter(url){
+    
 }
