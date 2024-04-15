2f4af724f3765d1d7276fce69e68340cce08de69
ZeilingerAlexander <116539631+ZeilingerAlexander@users.noreply.github.com>
2024-04-15 09:27:44 +0200

    update server on request to include post queries updated the procedure to handle query requests to not abbriviate between post and get

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // the node server
 import * as http from "http";
-import {HandleGetQuery} from "./server_requestHandlers/QueryHandlers.js";
+import {HandleQuery} from "./server_requestHandlers/QueryHandlers.js";
 import {IsRelativePathFile} from "./InputValidator.js";
 import {HandleGetFile, HandleNotFound} from "./server_requestHandlers/FileHandlers.js";
 import {HandleGetDirectoryNavigator} from "./server_requestHandlers/DirectoryHandlers.js";
 import {LogDebugMessage, LogErrorMessage} from "./logger.js";
+import {HandleAuthorizationOnRequest} from "./Authorization/auth.js";
 
 /*Starts the node server*/
 export async function StartServer (){
@@ -19,48 +20,68 @@ export async function StartServer (){
 /*Gets called on any server request*/
 async function on_ServerRequest(req, res){
     LogDebugMessage(`Got Request ${req.method} ${req.url}`);
+    
+    // Handle authentication, return if authentication fails
+    const authorization_success_message = await HandleAuthorizationOnRequest(req, res).catch(
+        (err) => LogErrorMessage("Authorization failed",err)
+    );
+    if (!authorization_success_message){
+        return;
+    }
+    
+    // Handle A Query Request on /GET/, /POST/, etc
+    if (req.url.startsWith("/GET/") || req.url.startsWith("/POST/")){
+            const complete_message = await HandleQuery(req, res).catch(
+                (err) => LogErrorMessage(err.message, err)
+            );
+            if (complete_message){
+                return resolve("Handling query completed successfully", complete_message);
+            }
+            else{
+                return reject("Handling query failed");
+            }
+    }
+    
     if (req.method === "GET"){
         await on_ServerGetRequest(req, res).catch(
             (err) => LogErrorMessage("Handling Server Get Request failed" + err.message, err)
         ).then(
             (msg) => LogDebugMessage("Handling Server Get Request completed " + msg)
         );
-        
     }
+    else if (req.method === "POST"){
+        await on_ServerPostRequest(req, res).catch(
+            (err) => LogErrorMessage("Handling Server Post Request failed" + err.message, err)
+        ).then(
+            (msg) => LogDebugMessage("Handling Server Post Request completed " + msg)
+        );
+    }
+}
+
+/*Gets called no post request, handles it depending on the request*/
+async function on_ServerPostRequest(req,res){
+    return new Promise(async (resolve,reject) => {
+        return reject("Not implemented");
+    });
 }
 
 /*Gets called on every server get request
 * Handles calling the appropiate handlers depending on the request*/
 async function on_ServerGetRequest(req,res){
     return new Promise(async (resolve, reject) => {
-        // Check if request is a non-file/folder request, just a normal query request
-        if (req.url.startsWith("/GET/")){
-            const complete_message = await HandleGetQuery(req, res).catch(
-                (err) => LogErrorMessage(err.message, err)
-            );
-            if (complete_message){
-                return resolve("Handling Get query completed successfully", complete_message);
-            }
-            else{
-                return reject("Handling Get query failed");
-            }
+        // Request is a file/directory get request, so check the path
+        // Request could also be something else but nothing is configured above
+        // On Request error return 404 Page
+        const complete_message = await HandleGetContent(req, res).catch(
+            async (err) => {
+                await LogErrorMessage(err.message, err);
+                await HandleNotFound(req, res);
+            })
+        if (complete_message){
+            return resolve("Handling Get content completed ", complete_message);
         }
         else{
-            // Request is a file/directory get request, so check the path
-            // Request could also be something else but nothing is configured above
-            // On Request error return 404 Page
-            const complete_message = await HandleGetContent(req, res).catch(
-                async (err) => {
-                    await LogErrorMessage(err.message, err);
-                    await HandleNotFound(req, res);
-                }
-            )
-            if (complete_message){
-                return resolve("Handling Get content completed ", complete_message);
-            }
-            else{
-                return reject("Handling Get content failed");
-            }
+            return reject("Handling Get content failed");
         }
     });
 }
