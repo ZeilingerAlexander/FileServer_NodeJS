2582f70fd8f5d36abef1e1d181650e27e3ced6d0
ZeilingerAlexander <116539631+ZeilingerAlexander@users.noreply.github.com>
2024-04-15 10:57:20 +0200

    finished getting the cookie in backend, this works by setting the Path property since that indicates what the req path must start with in order for the cookie to be sent

--- server_requestHandlers/POSTEndpoints/PostAuthorizationAuthorize.js
+++ server_requestHandlers/POSTEndpoints/PostAuthorizationAuthorize.js
@@ -2,7 +2,7 @@
 /*Handles the authorization post request, returns reject on invalid credentials*/
 export async function HandleAuthorizationOnPost(req,res){
     return new Promise(async (resolve,reject) => {
-        res.writeHead(200, {"Set-Cookie" : "Authorization=test; SameSite=Lax"});
+        res.writeHead(200, {"Set-Cookie" : "Authorization=test; SameSite=Lax;Path=/"});
         res.end();
         resolve("test-ignore");
     });
