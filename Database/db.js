f81f76448e59616c2f3c57fd4857ebd863134840
ZeilingerAlexander <116539631+ZeilingerAlexander@users.noreply.github.com>
2024-04-18 06:48:44 +0200

    add error handling to add log entry

--- Database/db.js
+++ Database/db.js
@@ -42,3 +42,38 @@ export async function IsLoginValid(username, passwordHash){
         return resolve(data.length > 0);
     });
 }
+
+/*Generates an authentication token for the provided userid and adds it to the database marked as NOT-expired*/
+export async function GenerateAuthenticationToken(userid){
+    return new Promise(async (resolve,reject) => {
+        // TODO : implement
+        
+    });
+}
+
+/*Expires all authentication for the provided user id*/
+export async function ExpireAllAuthenticationTokensForUser(userid){
+    return new Promise(async (resolve,reject) => {
+        // TODO : implement
+    });
+}
+
+/*Adds a log entry with the provided data*/
+export async function AddLogEntry(message, ip, userid_nullable, accessToken_nullable, accessType_nullable){
+    return new Promise(async (resolve,reject) => {
+        if (!message || !ip){
+            return reject("Message and ip cant be empty");
+        }
+        const query = "INSERT INTO authentication.accesslog (time_creation,message,ip,user,accesstoken,accesstype)" +
+            "VALUES (?,?,?,?,?,?)";
+        const complete_message = await dbcontext.promise().query(query, [Date.now(), message,ip,userid_nullable,accessToken_nullable,accessType_nullable]).catch(
+            (err) => LogErrorMessage(err.message,err));
+        if (!complete_message){
+            return reject("Inserting Access log entry failed");
+        }
+
+
+        return resolve("Inserted Access log entry completed");
+    });
+}
+
