97f7755016d4d0987a8a217adaa688362307f447
ZeilingerAlexander <116539631+ZeilingerAlexander@users.noreply.github.com>
2024-05-13 14:44:46 +0200

    made code mor ambigous for re-usability

--- server_requestHandlers/FileHandlers.js
+++ server_requestHandlers/FileHandlers.js
@@ -5,6 +5,7 @@ import {MIME_TYPES} from "../variables/mimeTypes.js";
 import * as path from "path";
 import * as fs from "fs";
 import {LogErrorMessage} from "../logger.js";
+import {reject} from "bcrypt/promises.js";
 
 /*Handles Getting the File for the Provided Request, DO NOT CALL WITH UNCHECKED INPUT, THIS WILL NOT VALIDATE INPUT FOR YOU*/
 export async function HandleGetFile(req, res){
@@ -17,14 +18,24 @@ export async function HandleGetFile(req, res){
         // Write Result Head
         res.writeHead(statusCode, {"Content-Type" : mimeType});
         
+        const response_message = await WriteFileFromStaticPathToResult(res, contentPath).catch((err) => LogErrorMessage(err.message,err));
+        if (!response_message) {return reject("Failed to pipe filestream to result");}
+        return resolve("successfuly piped file stream to result");
+    });
+}
+
+/*Does exactly what the name says pipes static path file stream to result, rejects on failure
+* THIS DOES NOT WRITE THE RESULT HEAD, ALL THIS DOES IS PIPE THE FILE STREAM DO NOT EXPECT ANYTHING ELSE*/
+export async function WriteFileFromStaticPathToResult(res, static_path){
+    return new Promise(async (resolve,reject) => {
         // pipe file stream to result, then return
         try{
-            fs.createReadStream(contentPath).pipe(res);
+            fs.createReadStream(static_path).pipe(res);
             return resolve("Piping File Stream was successful");
         } catch (ex) {
             await LogErrorMessage(ex.message, ex);
             return reject("Piping File Stream failed ", ex.message);
-        }
+        } 
     });
 }
 
