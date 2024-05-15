ff8b2052ebfc9895f58dfdd036eaa0001625bf14
ZeilingerAlexander <116539631+ZeilingerAlexander@users.noreply.github.com>
2024-05-15 10:58:15 +0200

    added temp marker files to zip function

--- InputValidator.js
+++ InputValidator.js
@@ -405,7 +405,31 @@ export async function RemoveFile(file_path){
     });
 }
 
-/*Zips the Provided Directory to the provided output path, resolves when complete, rejects if any values empty or out_path already existing*/
+/*Removes the provided path if ti points to a file, rejects on any failure*/
+async function RemoveFile_WithErrors(){
+    return new Promise (async (resolve,reject) => {
+        const DoesPathExist = await CheckIFPathExists(file_path).catch((err) => LogErrorMessage(err.message,err));
+        if (DoesPathExist === undefined){
+            return reject("Failed to determine if path exists");
+        }
+        const isPathFile = await IsPathFile(file_path).catch((err) => LogErrorMessage(err.message,err));
+        if (isPathFile === undefined){
+            return reject("Failed to determine if path is file");
+        }
+        
+        if (DoesPathExist && isPathFile){
+            const response_msg = await fsp.unlink(file_path).catch((err) => LogErrorMessage(err.message,err));
+            if (!response_msg){
+                return reject("Failed to unlink file");
+            }
+        }
+        return resolve("finished unlinking");
+    });
+}
+
+
+/*Zips the Provided Directory to the provided output path, resolves when complete, rejects if any values empty or out_path already existing
+*  creates a temp file while the file is not finished zipping, removes it on completion*/
 export async function ZipDirectoryToPath(dir_to_zip, out_path){
     return new Promise (async (resolve,reject) => {
         if (!dir_to_zip || !out_path || await CheckIFPathExists(out_path)){
@@ -416,14 +440,56 @@ export async function ZipDirectoryToPath(dir_to_zip, out_path){
         const archive = archiver.create("zip", {
             zlib : {level: 1}
         });
-        archive.on("close", function (){
+        
+        // on close remove marker + resolve, or reject if marker removing fails
+        archive.on("close", async function (){
+            // try to remove the marker up to five times since zipping might have been costly, 
+            // if after that it still fails to remove just give up and reject
+            let marker_remove_retry_attempts = 5;
+            let marker_remove_retry_timout_between_attempts = 500;
+            
+            // will be defined on remove success
+            let marker_remove_response = undefined;
+            
+            while (marker_remove_retry_attempts > 0 && !marker_remove_response){
+                marker_remove_response = await Zipper_RemoveTempFileMarker(out_path).catch((err) => LogErrorMessage(err.message,err));
+                if (marker_remove_response){
+                    // success
+                    break;
+                }
+                // no success, wait timeout and retry if enough attempts remaining
+                const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+                await delay(marker_remove_retry_timout_between_attempts)
+                marker_remove_retry_attempts = marker_remove_retry_attempts - 1;
+            }
+            
+            // if marker remove response still undefined give up and reject
+            if (!marker_remove_response){
+                return reject("Failed to remove marker, giving up");
+            }
+            
             return resolve("Successfully saved to zip file");
         });
 
-        archive.on("error", function (err){
+        // on error remove zip and marker + reject
+        archive.on("error", async function (err){
             LogErrorMessage(err.message,err);
+            
+            // remove zip file, if that works remove the old marker for it
+            const remove_response_msg = await RemoveFile_WithErrors(out_path).catch((err) => LogErrorMessage(err.message,err));
+            if (remove_response_msg){
+                // also remove marker but keep it so further reads will know that it points to a bad zip file,
+                // it doesnt matter if it fails or not since its just the marker and we reject anyways
+                await Zipper_RemoveTempFileMarker(out_path).catch((err) => LogErrorMessage(err.message,err));
+            }
             return reject("Failed to compress to zip file");
         });
+        
+        // create marker before piping to output since that will already create the zip file which might or might not fail
+        const zipper_create_respone = await Zipper_CreateTempFileMarker(out_path).catch((err) => LogErrorMessage(err.message, err));
+        if (!zipper_create_respone){
+            return reject("Failed to create temp file marker");
+        }
         
         archive.pipe(out);
 
@@ -445,6 +511,32 @@ export async function ZipDirectoryToPath(dir_to_zip, out_path){
         }
         
         archive.finalize();
+    });
+}
+
+
+/*Creates a temp file marker for the zip file residing on provided path, this marker indicates that the file is currently being written
+* rejects on error*/
+async function Zipper_CreateTempFileMarker(zipPath){
+    return new Promise(async (resolve,reject) => {
+        fs.writeFile(zipPath+process.env.ZIPPER_TEMPFILEMARKEREXTENTION, "", err => {
+            if (err){
+                LogErrorMessage(err.message, err);
+                return reject("Failed to create temp file");
+            }
+            return resolve("Wrote file successfully");
+        });
+    });
+}
+
+/*Removes the temp file marker for the zip file residing in the provided path, removing the marker indicates that the file was written without errors*/
+async function Zipper_RemoveTempFileMarker(zipPath){
+    return new Promise (async (resolve,reject) => {
+        const response_msg = await RemoveFile_WithErrors(zipPath+process.env.ZIPPER_TEMPFILEMARKEREXTENTION).catch((err) => LogErrorMessage(err.message,err));
+        if (!response_msg){
+            return reject("Failed to remove temp file marker");
+        }
+        return resolve("Succesfully remove temp file marker");
     });
 }
 
